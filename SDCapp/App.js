b02ef05ce86a9db73480db89459f5b0372352c39
Cortts Limited <corttslimited@nurudeens-iMac.local>
2019-05-01 17:00:38 +0100

    added react navigation

--- SDCapp/App.js
+++ SDCapp/App.js
@@ -1,21 +1,11 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {createStackNavigator, createAppContainer} from 'react-navigation';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+const MainNavigator = createStackNavigator({
+  Fisrt: {screen: FirstScreen},
+  Details: {screen: DetailScreen},
+  Last: {screen: LastScreen},
 });
+
+const App = createAppContainer(MainNavigator);
+
+export default App;
---
+++ SDCapp/components/textInputs.js
@@ -0,0 +1,4 @@
+import React,  {Components}  from 'react';
+import { AppRegistry, textInput} from 'react-native' 
+import { Icon } from 'react-native-elements'
+
