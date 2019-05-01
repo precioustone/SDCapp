eb3a8a74745e9aa693b7bd208fcff92c6796ffdf
Cortts Limited <corttslimited@nurudeens-iMac.local>
2019-05-01 20:27:57 +0100

    added oldPatients screen

--- SDCapp/screens/firstScreen.js
+++ SDCapp/screens/firstScreen.js
@@ -1,11 +1,25 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
-export default class App extends React.Component {
+export default class FirstScreen extends React.Component {
+
+  static navigationOptions = {
+    title: 'Welcome To SDC',
+  };
+
   render() {
     return (
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
+        <View>
+          <Button
+            title= "New Patient"
+            onPress={() => navigate('Details')}
+          />
+          <Button
+            title= "New Patient"
+          />
+        </View>
       </View>
     );
   }
