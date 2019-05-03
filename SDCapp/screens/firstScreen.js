10d0db0d0a311501c82dcf27f12f740bbe67ef57
Cortts Limited <corttslimited@nurudeens-iMac.local>
2019-05-03 12:07:25 +0100

    Added Linear Gradient and react-native-elements

--- SDCapp/screens/firstScreen.js
+++ SDCapp/screens/firstScreen.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Button, StyleSheet, Text, View } from 'react-native';
+import { LinearGradient } from 'expo';
+import { Avatar } from 'react-native-elements';
 
 export default class FirstScreen extends React.Component {
 
@@ -10,35 +12,54 @@ export default class FirstScreen extends React.Component {
   render() {
     const {navigate} = this.props.navigation;
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <View style={styles.row}>
-          <View style={styles.container}>
-            <Button
-              title= "New Patient"
-              onPress={() => navigate('Details')}
-            />
-          </View>
-          <View style={styles.container}>
-            <Button
-              title= "Old Patient"
-              onPress={() => navigate('Old')}
-            />
+      <LinearGradient
+        colors= {['#00b4db', '#0083b0']}
+        style={styles.gradient}>
+        <View style={styles.container}>
+
+        <Avatar
+          rounded
+          source={require('../assets/sdc.jpg')}
+        />
+
+
+          <View style={styles.row}>
+            <View style={styles.container}>
+              <Button
+                title= "New Patient"
+                onPress={() => navigate('Details')}
+              />
+            </View>
+            <View style={styles.container}>
+              <Button
+                style= {{color: '#f00'}}
+                title= "Old Patient"
+                onPress={() => navigate('Old')}
+              />
+            </View>
           </View>
         </View>
-      </View>
+      </LinearGradient>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
+  gradient: {
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 15,
+  },
+  container: {
+    flex: 0.7,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#fff',
+    borderRadius: 10,
   },
   row: {
-    flex: 1,
+    
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'center',
