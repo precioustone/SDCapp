40ed6402b35aa064440cbadfd0925bc4073401ae
precioustone <preciousidam@yahoo.com>
2019-05-27 20:06:44 +0100

    added touchableopacity

--- SDCapp/screens/firstScreen.js
+++ SDCapp/screens/firstScreen.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { LinearGradient } from 'expo';
-import { Avatar } from 'react-native-elements';
+//import { Avatar } from 'react-native-elements';
 
 export default class FirstScreen extends React.Component {
 
@@ -12,59 +12,67 @@ export default class FirstScreen extends React.Component {
   render() {
     const {navigate} = this.props.navigation;
     return (
-      <LinearGradient
-        colors= {['#00b4db', '#0083b0']}
-        style={styles.gradient}>
-        <View style={styles.container}>
-
-        <Avatar
-          rounded
-          source={require('../assets/sdc.jpg')}
-        />
-
-
-          <View style={styles.row}>
-            <View style={styles.container}>
-              <Button
-                title= "New Patient"
-                onPress={() => navigate('Details')}
-              />
+      <View style={styles.container}>
+        <TouchableOpacity 
+         onPress={() => navigate('Details')}
+         style={styles.touchable}>
+          <LinearGradient
+            colors={['#00b4db','#0083b0']}
+            style={styles.gradient}>
+            <View style={styles.button}>
+              <Text style={styles.buttonText}>NEW PATIENT</Text>
             </View>
-            <View style={styles.container}>
-              <Button
-                style= {{color: '#f00'}}
-                title= "Old Patient"
-                onPress={() => navigate('Old')}
-              />
-            </View>
+          </LinearGradient>
+        </TouchableOpacity>
+
+        <TouchableOpacity 
+          onPress={() => navigate('Details')}
+          style={styles.touchable}>
+          <View style={styles.buttonTwo}>
+            <Text style={styles.buttonTextTwo}>RETURN PATIENT</Text>
           </View>
-        </View>
-      </LinearGradient>
+        </TouchableOpacity>
+      </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  gradient: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 15,
-  },
   container: {
-    flex: 0.7,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#fff',
-    borderRadius: 10,
-  },
-  row: {
-    
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  buttonContainer: {
     flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  touchable: {
+    marginTop: 50,
+    borderRadius: 50,
+    borderWidth: 1,
+    borderColor: '#fff',
+  },
+  gradient: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 50,
+    borderWidth: 1,
+    borderColor: '#fff',
+  },
+  button: {
+    width: 260,
+    alignItems: 'center',
+  },
+  buttonTwo: {
+    width: 260,
+    alignItems: 'center',
+    borderRadius: 50,
+    borderWidth: 1,
+    borderColor: '#00b4db',
+  },
+  buttonText: {
+    color: 'white',
+    padding: 20,
+  },
+  buttonTextTwo: {
+    color: '#0083b0',
+    padding: 20,
   }
 });
